676a99d6036d8fbbda4a780f1e65c8dee52b2a1a
geroge282 <jorge_gueo@hotmail.com>
2019-11-22 18:50:17 -0500

    CVCVCV

--- app/src/main/java/com/example/semana6b/MainActivity.java
+++ app/src/main/java/com/example/semana6b/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //prueba mundo 2
+        //prueba mundo 2 3
     }
 }
